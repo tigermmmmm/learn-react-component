bbeacd4c3337fdf876335a2f0d047c2c2fccd61b
Tiger <1124915947@qq.com>
2021-03-04 23:49:04 +0800

    【update】函数组件memo的使用

--- src/09_React性能优化/05_memo的使用.js
+++ src/09_React性能优化/05_memo的使用.js
@@ -1,44 +1,53 @@
-import React, { Component } from "react";
-
-function Header() {
+import React, { PureComponent, memo } from "react";
+// 函数式组件阻止重新渲染
+const MemoHeader = memo(function Header() {
     console.log('Header被调用');
     return <h2>我是Footer组件</h2>;
-}
-class Banner extends Component {
+})
+
+class Banner extends PureComponent {
     render() {
         console.log('Banner render被调用');
         return <h2>我是Banner组件</h2>
     }
 }
-function ProductList() {
-    console.log('ProductList被调用');
-    return (
-        <ul>
-            <li>商品列表1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-        </ul>
-    );
-}
-class Main extends Component {
+
+const MemoProductList = memo(
+    function ProductList() {
+        console.log('ProductList被调用');
+        return (
+            <ul>
+                <li>商品列表1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        );
+    }
+)
+
+class Main extends PureComponent {
     render() {
         console.log('Main render被调用');
         return (
             <div>
                 <Banner />
-                <ProductList />
+                <MemoProductList />
             </div>
         )
     }
 }
-function Footer() {
-    console.log('Footer被调用');
-    return <h2>我是Footer组件</h2>;
-}
 
-export default class App extends Component {
+const MemoFooter = memo(
+    function Footer() {
+        console.log('Footer被调用');
+        return <h2>我是Footer组件</h2>;
+    }
+)
+
+
+export default class App extends PureComponent {
     constructor(props) {
         super(props);
 
@@ -52,9 +61,9 @@ export default class App extends Component {
             <div>
                 <div>当前计数： {this.state.count}</div>
                 <button onClick={e => this.increment()}>+1</button>
-                <Header />
+                <MemoHeader />
                 <Main />
-                <Footer />
+                <MemoFooter />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './01_组件的定义方式/App';
-import App from "./09_React性能优化/04_PureComponent"
+import App from "./09_React性能优化/05_memo的使用"
 
 ReactDOM.render(
   // <React.StrictMode>
